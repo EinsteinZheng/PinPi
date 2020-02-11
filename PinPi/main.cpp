b823ca0e3b2e8e8ada780412607ec429709bb2d3
Einstein Zheng <mocoder@icloud.com>
2020-02-11 21:23:19 +0800

    Initial Commit

---
+++ PinPi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PinPi
+//
+//  Created by Einstein Zheng on 2/11/20.
+//  Copyright © 2020 Mocoder Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
